969f941bd51cd30af4638d8695a50d75ce14a5b6
ChrisJipa <ciprian.jipa78@gmail.com>
2021-06-17 16:20:21 +0300

    added wording to the HelloWorldString

--- src/main/java/com/sda_store/controller/TestController.java
+++ src/main/java/com/sda_store/controller/TestController.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.HttpURLConnection;
-
 @RestController
 public class TestController {
 
@@ -16,6 +14,6 @@ public class TestController {
 
     @GetMapping(path = "/hello-world")
     public String HelloWorldString() {
-        return "HelloWorld";
+        return "Hello World! This is the SDA test controller!";
     }
 }
